d908000db53f4c9e9295ae5d1ea5cc89b4b3c108
Hamzat <hamti2018@gmail.com>
2021-12-17 03:01:07 +0300

    fix

--- src/bot.js
+++ src/bot.js
@@ -10,13 +10,6 @@ const throttler = apiThrottler()
 const bot = new Bot(process.env.BOT_TOKEN)
 
 bot.api.config.use(throttler)
-bot.filter(ctx => filter(ctx))
-bot.use(...commands)
-bot.catch((err) => {
-  const ctx = err.ctx
-  console.error(`Error while handling update ${ctx.update.update_id}:`)
-})
-
 bot.use(async (ctx, next) => {
   const id = ctx.from?.id
   // eslint-disable-next-line no-eval
@@ -25,6 +18,12 @@ bot.use(async (ctx, next) => {
   }
 
   await ctx.reply('Вы не админ')
+})
+
+bot.use(...commands)
+bot.catch((err) => {
+  const ctx = err.ctx
+  console.error(`Error while handling update ${ctx.update.update_id}:`)
 })
 
 // async function filter(ctx) {
